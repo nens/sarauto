7aa8261a3ab0b2fd2f495a753234522c935d9ef9
Wietze <wietze.suijker@nens.local>
2020-06-10 17:41:59 +0200

    Revert "Revert "naming & formatting""
    This reverts commit a68878c14872e93d02eae12f7ce4e0e077da5ec4.

--- step2_xml_2.py
+++ step2_xml_2.py
@@ -5,8 +5,8 @@ import logging
 
 logger = logging.getLogger()
 
-isFile = os.getcwd()+config.xmlprocesspath
-listOfFiles=os.listdir(isFile)
+xml_process_path = os.getcwd()+config.xmlprocesspath
+SNAP_io_files = os.listdir(xml_process_path)
 
 # Function to check File Dim exist or not in directory
 def get_xml_name(elem):
@@ -17,8 +17,8 @@ def get_xml_name(elem):
         except:
             pass
 
-for d_file in listOfFiles:
-    filepath = isFile + d_file
+for SNAP_io_file in SNAP_io_files:
+    filepath = xml_process_path + SNAP_io_file
     tree = etree.parse(filepath)
     elem = tree.findall(".//node")
     dim_name = get_xml_name(elem)
@@ -28,18 +28,18 @@ for d_file in listOfFiles:
         sys.exit(0)
     else:
         if os.path.exists(dim_name):
-            logger.info(os.path.basename(dim_name)+ ' already processed')
-            print (os.path.basename(dim_name) + ' already processed')
+            logger.info(os.path.basename(dim_name)+ " already processed")
+            print (os.path.basename(dim_name) + " already processed")
             os.remove(filepath)
         else:
             logger.info("processing GPT for "+os.path.basename(dim_name))
             print("processing GPT for "+os.path.basename(dim_name))
             try:
                 os.system("gpt "+filepath)
-                logger.info("GPT for "+os.path.basename(dim_name)+' is complete')
-                print("GPT for "+os.path.basename(dim_name)+' is complete')
+                logger.info("GPT for "+os.path.basename(dim_name)+" is complete")
+                print("GPT for "+os.path.basename(dim_name)+" is complete")
                 os.remove(filepath)
             except:
-                print('Failed to complete GPT for '+os.path.basename(dim_name))
-                logger.error('Failed to complete GPT for '+os.path.basename(dim_name))
+                print("Failed to complete GPT for "+os.path.basename(dim_name))
+                logger.error("Failed to complete GPT for "+os.path.basename(dim_name))
                 sys.exit(0)
